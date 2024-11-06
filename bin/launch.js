b40c76c62ed9396c5efbb7314de2e4807720c413
Omid Kashan <omid@antilong.com>
2024-11-06 20:24:53 +0000

    feat: unwrap nasty try catch block

--- bin/launch.js
+++ bin/launch.js
@@ -2,31 +2,35 @@ import puppeteer from "puppeteer"
 import { readFile } from "node:fs/promises"
 import { build } from "./build.js"
 
-export async function launch({ experiment, script }) {
+async function loadConfig({ experiment }) {
   try {
-    const config = await readFile(`experiments/${experiment}/config.json`).then((res) =>
+    return await readFile(`experiments/${experiment}/config.json`).then((res) =>
       JSON.parse(res.toString())
     )
-
-    const { code } = await build({ experiment, script, write: false })
-
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-      devtools: true,
-    })
-    const page = await browser.newPage()
-
-    await page.goto(config.url)
-    await page.waitForNetworkIdle()
-    await page.waitForSelector("body")
-    await page.addScriptTag({ content: code })
-
-    page.on("domcontentloaded", async () => {
-      const { code } = await build({ experiment, script, write: false })
-      return await page.addScriptTag({ content: code })
-    })
   } catch (e) {
     console.log(e)
+    process.exit(1)
   }
+}
+
+export async function launch({ experiment, script }) {
+  const config = await loadConfig({ experiment })
+  const { code } = await build({ experiment, script, write: false })
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    devtools: true,
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto(config.url)
+  await page.waitForNetworkIdle()
+  await page.waitForSelector("body")
+  await page.addScriptTag({ content: code })
+
+  page.on("domcontentloaded", async () => {
+    const { code } = await build({ experiment, script, write: false })
+    return await page.addScriptTag({ content: code })
+  })
 }
