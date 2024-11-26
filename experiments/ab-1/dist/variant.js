847ac233a08f313e072c3d548aa5fc0841267b01
Omid Kashan <omid@antilong.com>
2024-11-26 17:50:41 +0000

    feat(ab-1): update example test files

--- experiments/ab-1/dist/variant.js
+++ experiments/ab-1/dist/variant.js
@@ -1,16 +1,1 @@
-!(function () {
-  "use strict"
-  console.log("hello"),
-    console.log("So helpful"),
-    document.querySelector("body").setAttribute("style", "border:10px solid pink")
-  const e = document.createElement("div")
-  ;(e.textContent = "I'M A TEST LOL"),
-    e.setAttribute(
-      "style",
-      "background:orange;padding:20px;position:absolute;top:20px;left:20px;width:100%;z-index:9999"
-    ),
-    console.log({ foo: "bar" }),
-    document.body.appendChild(e),
-    (document.querySelector(".coop-co-uk-hero__title").textContent =
-      "TAKE A LOOK AT THIS AMAZING THING")
-})()
+!function(){"use strict";console.log("hello"),console.log("So helpful"),document.querySelector("body").setAttribute("style","border:10px solid pink");const e=document.createElement("div");e.textContent="I'M A TEST LOL",e.setAttribute("style","background:orange;padding:20px;position:absolute;top:20px;left:20px;width:100%;z-index:9999"),console.log({foo:"bar"}),document.body.appendChild(e),document.querySelector(".coop-co-uk-hero__title").textContent="TAKE A LOOK AT THIS AMAZING THING";const t=document.createElement("style");t.textContent="body{background:green}",document.head.appendChild(t),document.body.innerHTML="<div>hello</div>\n"}();
