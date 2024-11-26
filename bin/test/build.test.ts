77c9a1709a8fba831d3968280310269c955eeca1
Omid Kashan <omid@antilong.com>
2024-11-26 20:23:17 +0000

    test: add more build tests
    Not thrilled about the way I'm checking which rollup method was called, but couldnt figure out a nice way to mock the inner generate/write functions. This does the job for now and ensures both branches are tested.

--- bin/test/build.test.ts
+++ bin/test/build.test.ts
@@ -7,13 +7,17 @@ const experiment = "__test__"
 
 vi.mock("rollup", async () => {
   return {
-    rollup: vi.fn().mockImplementation(() => {
+    rollup: vi.fn().mockImplementation((rollupOptions) => {
       return {
         generate: vi.fn().mockImplementation(async () => {
-          return { output: [{ fileName: "rollupOutput" }] }
+          return {
+            output: [{ fileName: rollupOptions.input.split("/").at(-1), __method: "generate" }],
+          }
         }),
         write: vi.fn().mockImplementation(async () => {
-          return { output: [{ fileName: "rollupOutput" }] }
+          return {
+            output: [{ fileName: rollupOptions.input.split("/").at(-1), __method: "write" }],
+          }
         }),
         close: vi.fn(),
       }
@@ -23,12 +27,24 @@ vi.mock("rollup", async () => {
 
 vi.spyOn(log, "log").mockImplementation(() => {})
 
+global.process = {
+  ...process,
+  // @ts-expect-error
+  exit: vi.fn(),
+}
+
 afterEach(() => {
   vi.clearAllMocks()
 })
 
-it("compiles a single variant when specified", async () => {
-  await builder.build({ experiment, variant: "variant-a.js", write: false })
+it("exits if no valid inputs are provided", async () => {
+  await builder.build({ experiment, variant: "variant-nope.js", write: false })
+  expect(global.process.exit).toHaveBeenCalledTimes(1)
+})
+
+it("compiles a single variant in memory when specified", async () => {
+  const output = await builder.build({ experiment, variant: "variant-a.js", write: false })
+
   expect(rollup.rollup).toHaveBeenCalledTimes(1)
   expect(rollup.rollup).toHaveBeenCalledWith(
     expect.objectContaining({ input: "./experiments/__test__/variant-a.js" })
@@ -36,15 +52,67 @@ it("compiles a single variant when specified", async () => {
   expect(rollup.rollup).not.toHaveBeenCalledWith(
     expect.objectContaining({ input: "./experiments/__test__/variant-b.js" })
   )
+
+  expect(output).toStrictEqual({
+    "variant-a.js": { fileName: "variant-a.js", __method: "generate" },
+  })
 })
 
-it("compiles multiple variants when variant is not specified", async () => {
-  await builder.build({ experiment, write: false })
+it("compiles multiple variants in memory when variant is not specified", async () => {
+  const output = await builder.build({ experiment, write: false })
   expect(rollup.rollup).toHaveBeenCalledTimes(2)
   expect(rollup.rollup).toHaveBeenCalledWith(
     expect.objectContaining({ input: "./experiments/__test__/variant-a.js" })
   )
   expect(rollup.rollup).toHaveBeenCalledWith(
     expect.objectContaining({ input: "./experiments/__test__/variant-b.js" })
+  )
+
+  expect(output).toStrictEqual({
+    "variant-a.js": { fileName: "variant-a.js", __method: "generate" },
+    "variant-b.js": { fileName: "variant-b.js", __method: "generate" },
+  })
+})
+
+it("compiles a single variant to disk when specified", async () => {
+  const output = await builder.build({ experiment, variant: "variant-a.js", write: true })
+
+  expect(rollup.rollup).toHaveBeenCalledTimes(1)
+  expect(rollup.rollup).toHaveBeenCalledWith(
+    expect.objectContaining({ input: "./experiments/__test__/variant-a.js" })
+  )
+  expect(rollup.rollup).not.toHaveBeenCalledWith(
+    expect.objectContaining({ input: "./experiments/__test__/variant-b.js" })
+  )
+
+  expect(output).toStrictEqual({
+    "variant-a.js": { fileName: "variant-a.js", __method: "write" },
+  })
+})
+
+it("compiles multiple variants to disk when variant is not specified", async () => {
+  const output = await builder.build({ experiment, write: true })
+  expect(rollup.rollup).toHaveBeenCalledTimes(2)
+  expect(rollup.rollup).toHaveBeenCalledWith(
+    expect.objectContaining({ input: "./experiments/__test__/variant-a.js" })
+  )
+  expect(rollup.rollup).toHaveBeenCalledWith(
+    expect.objectContaining({ input: "./experiments/__test__/variant-b.js" })
+  )
+
+  expect(output).toStrictEqual({
+    "variant-a.js": { fileName: "variant-a.js", __method: "write" },
+    "variant-b.js": { fileName: "variant-b.js", __method: "write" },
+  })
+})
+
+it("adds terser to plugin chain when minify is true", async () => {
+  await builder.build({ experiment, variant: "variant-a.js", write: true, minify: true })
+  expect(rollup.rollup).toHaveBeenCalledTimes(1)
+  expect(rollup.rollup).toHaveBeenCalledWith(
+    expect.objectContaining({
+      input: "./experiments/__test__/variant-a.js",
+      plugins: expect.arrayContaining([expect.objectContaining({ name: "terser" })]),
+    })
   )
 })
