4123e948820c5073cbbea716e8e84caeb250f191
Omid Kashan <omid.kashan@coop.co.uk>
2024-11-26 17:50:41 +0000

    feat(ab-1): update example test files

--- experiments/ab-1/variant.js
+++ experiments/ab-1/variant.js
@@ -1,11 +1,16 @@
 import { plop } from "./module"
 import { helper } from "../common/helper"
+
+import styles from "./style.css"
+import html from "./test.html"
+
 console.log("hello")
 helper()
 //window.addEventListener("DOMContentLoaded", () => {
 document.querySelector("body").setAttribute("style", "border:10px solid pink")
 
 const container = document.createElement("div")
+container.classList.add("test-container")
 container.textContent = "I'M A TEST LOL"
 container.setAttribute(
   "style",
@@ -15,5 +20,11 @@ console.log(plop)
 document.body.appendChild(container)
 
 document.querySelector(".coop-co-uk-hero__title").textContent = "TAKE A LOOK AT THIS AMAZING THING"
+const styleTag = document.createElement("style")
+
+styleTag.textContent = styles
+document.head.appendChild(styleTag)
+
+document.querySelector(".test-container").innerHTML = html
 
 //})
